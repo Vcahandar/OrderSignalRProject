309c186891fc3a8edd985d031f895522c6bd71b6
Vcahandar <jahandarmv@code.edu.az>
2024-06-12 01:47:34 +0400

    UI Layout Header and Footer ViewComponents done

--- SignalRProject/SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
+++ SignalRProject/SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c55d0c190ba6f8b83c4f5145f3b14497a397a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c41c51a3baf5046b283a40e835c0b092eaedc503")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
